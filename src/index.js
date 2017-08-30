f68c4d0a0e7cf74a05456c4b692e2944c8de41a0
Bala Raj <bala.raj@a-cti.com>
2017-08-30 17:44:27 +0530

    I think did the changes to make it work properly

--- src/index.js
+++ src/index.js
@@ -1,101 +1,66 @@
-// Demo component
-// this is only example component
 
-var INITIAL_MAP_ZOOM_LEVEL = 9;
+const INITIAL_MAP_ZOOM_LEVEL = 9;
 
-var ATLANTIC_OCEAN = {
-  latitude: 29.532804,
-  longitude: -55.491477
-};
-
-
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class MyComponent extends React.Component {    
-    constructor(props) {
-        super(props);
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-             isGeocodingError: false,             
-        }
-    }
+    this.state = {
+      isGeocodingError: false,
+    };
+  }
 
-    componentDidMount() {        
-   
+  componentDidMount() {
     this.geocoder = new window.google.maps.Geocoder();
-
-    this.geocoder.geocode({ 'address': this.props.address }, function handleResults(results, status) {
-
-      if (status === window.google.maps.GeocoderStatus.OK) {
-
-        this.setState({
-          foundAddress: results[0].formatted_address,
-          isGeocodingError: false
-        });
-        
-        new window.google.maps.Map(this.mapElement, {
-      zoom: INITIAL_MAP_ZOOM_LEVEL,
-      center: results[0].geometry.location,
-    });
-
-        return;
-      }
-
-      this.setState({
-        foundAddress: null,
-        isGeocodingError: true
-      });
-
-    }.bind(this));
-    }
-
-   handleFormSubmit (submitEvent) {
-    submitEvent.preventDefault();
-
-    var address = this.searchInputElement.value;
-
-    this.geocodeAddress(address);
+    this.renderMap();
   }
 
-
-    geocodeAddress(address) {
-    this.geocoder.geocode({ 'address': address }, function handleResults(results, status) {
-
-      if (status === window.google.maps.GeocoderStatus.OK) {
-
-        this.setState({
-          foundAddress: results[0].formatted_address,
-          isGeocodingError: false
-        });
-
-        this.map.setCenter(results[0].geometry.location);       
-        return;
-      }
-
-      this.setState({
-        foundAddress: null,
-        isGeocodingError: true
-      });
-
-      this.map.setCenter({
-        lat: ATLANTIC_OCEAN.latitude,
-        lng: ATLANTIC_OCEAN.longitude
-      });
-
-    }.bind(this));
+  componentWillReceiveProps(nextProps) {
+    if (this.props.address != nextProps.address) {
+      this.renderMap();
+    }
   }
 
-    render() {
-        this.state.isGeocodingError ? <p className="bg-danger">Address not found.</p> : <p className="bg-info">{this.state.foundAddress}</p>
-        return (
-            <div className="map" ref={(c) => (this.mapElement = c)} style={{height:"600px"}} /> 
-        )
+  handleResults(results, status) {
+    if (status === window.google.maps.GeocoderStatus.OK) {
+      this.setState({ isGeocodingError: false });
+
+      if (!this.map) {
+        this.map = new window.google.maps.Map(this.mapElement, Object.assign({ center: results[0].geometry.location }, this.props.options));
+      } else {
+        this.map.setCenter(results[0].geometry.location);
+      }
+
+      return;
     }
+
+    this.setState({ isGeocodingError: true });
+  }
+
+  renderMap() {
+    this.geocoder.geocode({ address: this.props.address }, this.handleResults.bind(this));
+  }
+
+  render() {
+    this.state.isGeocodingError ? <p className="bg-danger">Address not found.</p> : <p className="bg-info">{this.state.foundAddress}</p>;
+    return (
+      <div className="map" ref={c => (this.mapElement = c)} style={{ height: '600px' }} />
+    );
+  }
 }
 
 MyComponent.propTypes = {
-    address: PropTypes.string.isRequired,    
-  }
+  address: PropTypes.string.isRequired,
+  options: PropTypes.object,
+};
+
+MyComponent.defaultProps = {
+  options: {
+    zoom: INITIAL_MAP_ZOOM_LEVEL,
+  },
+};
 
 export default MyComponent;
